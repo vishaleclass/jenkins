a92913e4aebdd46e77212ee34590b72e262d52d5
vishal <33727501+vishaleclass@users.noreply.github.com>
2019-05-07 13:24:01 +0530

    Update nodejsdocker.groovy

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('vishaleclass/vishal')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('vishaleclass')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
